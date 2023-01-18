759340d3aa57a1eb4f640bd6564e229ac5202667
Mohamed Shaikhon <mohamed.shaikhon94@gmail.com>
2023-01-18 12:52:12 +0200

    ignore pods

--- Pods/Target Support Files/Pods-RichNotificationService-weInternet/Pods-RichNotificationService-weInternet-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RichNotificationService_weInternet : NSObject
-@end
-@implementation PodsDummy_Pods_RichNotificationService_weInternet
-@end
